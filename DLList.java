bc8941bed03c7fad9859f77557732ce0781302f7
MannyXXX19 <73254412+MannyXXX19@users.noreply.github.com>
2020-11-12 16:59:48 -0800

    Add files via upload

--- DLList.java
+++ DLList.java
@@ -8,20 +8,19 @@ public class DLList<E> implements ListInterface<E> {
 	//look into LLNode or DLLNode
 	protected DLLNode<E> head;
 	protected DLLNode<E> tail;
-	protected DLLNode<E> current;
+	protected E current;
+	private boolean found = false;
 	
 
 	@Override
 	public E get(E element) {
 		// TODO Auto-generated method stub
-		DLLNode<E> current = head.getNext();
-		while(current != null) {
-			if(current.getInfo().equals(element))
-				return current.getInfo();
-			
-			current = current.getNext();
+		find(element);
+		if(found == true){
+		    return current;
 		}
-		return null;
+		else {	
+		    return null;
 	}
 
 	public String toString() {
