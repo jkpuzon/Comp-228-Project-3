c8e06fd682e75c262ddc47ec5cd5abe25e7a000d
bellabedore <73254402+bellabedore@users.noreply.github.com>
2020-11-16 10:18:14 -0500

    Update Project3Add.java

--- Project3Add.java
+++ Project3Add.java
@@ -1,5 +1,27 @@
-package PRACTICE;
-
-public class Project3Add {
-
-}
+public static add(E element) {
+		LLNode current;
+		
+		LLNode<E> newNode = new LLNode<E>(element);
+		if (head == null) {
+            head = newNode;
+		}
+		else if (head.getInfo().compareTo(newNode.getInfo()) > 0){
+			newNode.setNext(head);
+			newNode.getPrev().setInfo(newNode);
+			head= newNode;
+		}
+		else {
+			current = head;
+			 while(current.getNextItem() != null && current.getNextItem() < newNode.getInfo()) {
+				 current = current.getNextItem();
+			 }
+			newNode.setNext(current.getNextItem());
+			
+			if (current.getNextItem() != null) {
+				newNoew.getNextItem.setPrevItem(newNode);
+			}
+			current.setNextItem(newNode);
+			newNode.setPreV(current);
+			 
+		}
+  }
